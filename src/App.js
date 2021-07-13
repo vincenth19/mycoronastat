8d0d35f952b40d8f98a623b3ff31d51ecd9e9a89
vincenth19 <vincenthary19@gmail.com>
2021-07-13 13:51:49 +0800

    initial commit

--- src/App.js
+++ src/App.js
@@ -20,18 +20,7 @@ function App() {
           <ColorModeSwitcher justifySelf="flex-end" />
           <VStack spacing={8}>
             <Logo h="40vmin" pointerEvents="none" />
-            <Text>
-              Edit <Code fontSize="xl">src/App.js</Code> and save to reload.
-            </Text>
-            <Link
-              color="teal.500"
-              href="https://chakra-ui.com"
-              fontSize="2xl"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Learn Chakra
-            </Link>
+            <Text>Still work in progress. Please wait.</Text>
           </VStack>
         </Grid>
       </Box>
